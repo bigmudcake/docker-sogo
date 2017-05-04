141ca754710d1394c8ae1de2bf95d3cafad48cd1
bigmudcake <bigmudcake@gmail.com>
2017-05-04 18:20:20 +1000

    setup sogo.log file within srv folder correctly

--- sogod.sh
+++ sogod.sh
@@ -24,10 +24,9 @@ chown sogo:sogo /srv
 chmod 0775 /srv
 
 # edave - setup log file within srv folder
-touch /srv/sogo.log
-chown sogo:sogo /srv/sogo.log
-chmod 0777 /srv/sogo.log
+mkdir -p /srv/log
+chown -R sogo:sogo /srv/log
+chmod -R 0755 /srv/log
 
 # Run SOGo in foreground
-exec /sbin/setuser sogo /usr/sbin/sogod -WONoDetach YES -WOPidFile /var/run/sogo/sogo.pid 
-# -WOLogFile /srv/sogo.log
+exec /sbin/setuser sogo /usr/sbin/sogod -WONoDetach YES -WOPidFile /var/run/sogo/sogo.pid -WOLogFile /srv/log/sogo.log
