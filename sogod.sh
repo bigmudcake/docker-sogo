38dbbe9aace38e91650fd5722e7e313c205792dd
edave <edave@adagency.com.au>
2020-05-31 19:26:23 +1000

    fix orig config file creation

--- sogod.sh
+++ sogod.sh
@@ -1,29 +1,32 @@
 #!/bin/sh
 
-# Copy distribution config files to /srv as example
+# edave - Create orig config files if not exist
+if [ ! -f "/etc/sogo.conf.orig" ]; then
+    cp /etc/cron.d/sogo /etc/cron.orig
+    cp /etc/sogo/sogo.conf /etc/sogo.conf.orig
+fi
+
+# edave - Copy orig config files to /srv as example
 mkdir -p /srv/etc
-cp /etc/sogo/sogo.conf /srv/etc/sogo.conf.orig
+cp /etc/*.orig /srv/etc/
 
 # Copy back administrator's configuration
-cp /srv/etc/sogo.conf /etc/sogo/sogo.conf
+cp /srv/etc/sogo.conf /etc/sogo/sogo.conf 2>/dev/null
 chown sogo:sogo /etc/sogo/sogo.conf
 
 # Create SOGo home directory if missing
 mkdir -p /srv/lib/sogo
 chown sogo:sogo /srv/lib/sogo
 
-# Copy crontab to /srv as example
-cp /etc/cron.d/sogo /srv/etc/cron.orig
-
 # Load crontab
-cp /srv/etc/cron /etc/cron.d/sogo
+cp /srv/etc/cron /etc/cron.d/sogo 2>/dev/null
 chmod +x /usr/share/doc/sogo/*.sh
 
 # edave - set correct access for srv folder
 chown sogo:sogo /srv
 chmod 0775 /srv
 
-# edave - setup log file within srv folder
+# edave - setup log file folder within srv folder
 mkdir -p /srv/log
 chown -R sogo:sogo /srv/log
 chmod -R 0755 /srv/log
