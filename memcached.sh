22eb243e407ad8869cdeee8cddd1504c201156b6
edave <edave@adagency.com.au>
2020-06-01 20:54:46 +1000

    fix log file output

--- memcached.sh
+++ memcached.sh
@@ -4,4 +4,4 @@
 mkdir -p /srv/log
 
 # edave - start memcached daemon
-exec /sbin/setuser sogo /usr/bin/memcached -m ${memcached:-64} -s /tmp/memcached.sock -a 0700 >> /srv/log/memcached.log 2>&1
+exec /sbin/setuser sogo /usr/bin/memcached -m ${memcached:-64} -s /tmp/memcached.sock -a 0700 >>/srv/log/memcached.log 2>&1
