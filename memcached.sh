e5eb882e68e502ad24f3cf3967196c271cd53eb7
edave <edave@adagency.com.au>
2020-06-02 08:09:54 +1000

    update log file coding

--- memcached.sh
+++ memcached.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-# edave - setup log file folder within srv folder
-mkdir -p /srv/log
-
 # edave - start memcached daemon
-exec /sbin/setuser sogo /usr/bin/memcached -m ${memcached:-64} -s /tmp/memcached.sock -a 0700 >>/srv/log/memcached.log 2>&1
+exec /sbin/setuser sogo /usr/bin/memcached -m ${memcached:-64} -s /tmp/memcached.sock -a 0700 > /srv/log/memcached-startup.log 2>&1
