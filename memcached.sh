31cd12499185c47b32b527f72387efa222726113
edave <edave@adagency.com.au>
2020-06-01 20:44:50 +1000

    setup memcached log in srv folder

--- memcached.sh
+++ memcached.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-exec /sbin/setuser sogo /usr/bin/memcached -m ${memcached:-64} -s /tmp/memcached.sock -a 0700 >>/srv/log/memcached.log 2>&1
+
+# edave - setup log file folder within srv folder
+mkdir -p /srv/log
+
+# edave - start memcached daemon
+exec /sbin/setuser sogo /usr/bin/memcached -m ${memcached:-64} -s /tmp/memcached.sock -a 0700 >> /srv/log/memcached.log 2>&1
