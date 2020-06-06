59b4cc5d2baec0c1e6e1c373763140a02a465929
edave <edave@adagency.com.au>
2020-06-07 09:40:00 +1000

    change how service daemons are installed

--- init_startup.sh
+++ init_startup.sh
@@ -4,13 +4,12 @@
 GATEWAY=`ip route show 0.0.0.0/0 | awk '{print $3}'`
 echo "${GATEWAY} GATEWAY" >> /etc/hosts
 
-# edave - setup necessary srv subfolders and permissions
+# edave - setup necessary subfolders and permissions
 mkdir -p /srv/etc
 chown -R sogo:sogo /srv
 chmod -R 775 /srv
-
-# edave - allow log files to be viewed by non-root users
 chmod -R 775 /var/log
+chmod -R 775 /runfiles
 
 # edave - Create orig config files if not exist
 if [ ! -f "/etc/sogo.conf.orig" ]; then
@@ -19,22 +18,24 @@ if [ ! -f "/etc/sogo.conf.orig" ]; then
     cp /etc/sogo/sogo.conf /etc/sogo.conf.orig
 fi
 
-# edave - Copy orig config files to /srv as example
+# edave - Copy orig config files to /srv as config examples
 cp /etc/*.orig /srv/etc/
 
+# edave - setup main SOGo service daemon from runfiles
+rm -rf /etc/service/sogod
+mkdir -p /etc/service/sogod
+cp /runfiles/run_sogod.sh /etc/service/sogod/run
 
-# edave - enable/disable execute startup for memcached
-if [ "${memcached}" = "false" ]; then
-        MOD=-x
-else
-        MOD=+x
+# edave - setup optional memcached service daemon from runfiles
+rm -rf /etc/service/memcached
+if [ ! "${memcached}" = "false" ]; then
+    mkdir -p /etc/service/memcached
+    cp /runfiles/run_memcached.sh /etc/service/memcached/run
 fi
-chmod ${MOD} /etc/service/memcached/run
 
-# edave - enable/disable execute startup for Apache Web Server
+# edave - setup optional apache2 service daemon from runfiles
+rm -rf /etc/service/apache2
 if [ -f /srv/etc/apache-SOGo.conf ]; then
-        MOD=+x
-else
-        MOD=-x
+    mkdir -p /etc/service/apache2
+    cp /runfiles/run_apache2.sh /etc/service/apache2/run
 fi
-chmod ${MOD} /etc/service/apache2/run
