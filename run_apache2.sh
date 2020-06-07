88b4b4899e901f5c9dfc83f8e0ffa7eb822fb373
edave <edave@adagency.com.au>
2020-06-07 10:58:58 +1000

    remove conditional start of server

--- run_apache2.sh
+++ run_apache2.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
+# edave - Copy back and enable administrator's configuration
+cp /srv/etc/apache-SOGo.conf /etc/apache2/conf-enabled/SOGo.conf
 
-# Only run apache if config file exists
-if [ -f "/srv/etc/apache-SOGo.conf" ]; then
-
-    # Copy back and enable administrator's configuration
-    cp /srv/etc/apache-SOGo.conf /etc/apache2/conf-enabled/SOGo.conf
-
-    # Run apache in foreground
-    APACHE_ARGUMENTS="-DNO_DETACH" exec /usr/sbin/apache2ctl start
-fi
+# Run apache in foreground
+APACHE_ARGUMENTS="-DNO_DETACH" exec /usr/sbin/apache2ctl start
