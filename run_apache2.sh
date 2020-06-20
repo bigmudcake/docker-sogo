7de1ba39f354994aaa1128cd500c38335ecf6842
edave <edave@adagency.com.au>
2020-06-20 17:07:54 +1000

    Apache2 log folder setup

--- run_apache2.sh
+++ run_apache2.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# edave - Setup Apache2 logfile folder access
+echo "* run_apache2 - setup logfile folder /var/log/apache2"
+mkdir -p /var/log/apache2 2>/dev/null
+chmod -R 775 /var/log/apache2
+
 # edave - Copy back and enable administrator's udated configuration
 echo "* run_apache2 - update internal apache-SOGo.conf from /srv/etc"
 cp /srv/etc/apache-SOGo.conf /etc/apache2/conf-enabled/SOGo.conf
